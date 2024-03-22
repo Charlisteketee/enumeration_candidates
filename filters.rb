78d7a2fe375ea63735ba24dbbe8413ac3d62b772
Charlisteketee <charlisteketee@gmail.com>
2024-03-21 20:47:20 -0700

    Create test to find qualified candidates

--- filters.rb
+++ filters.rb
@@ -3,14 +3,40 @@
 
 def find(id)
     # Your code Here
+    @candidates.find { |candidate| candidate[:id] == id }
+
   end
   
   def experienced?(candidate)
-    # Your code Here
+    candidate[:years_of_experience] >= 2
+  end
+
+  def knows_ruby_or_python?(candidate)
+    candidate[:languages].include?('Ruby') || candidate[:languages].include?('Python')
+  end
+  
+  def applied_last_15_days?(candidate)
+    candidate[:date_applied] >= 15.days.ago.to_date
+  end
+  
+  def over_17?(candidate)
+    candidate[:age] > 17
   end
   
   def qualified_candidates(candidates)
-    # Your code Here
+    candidates.select do |candidate|
+      experienced?(candidate) &&
+        candidate[:github_points] >= 100 &&
+        knows_ruby_or_python?(candidate) &&
+        applied_last_15_days?(candidate) &&
+        over_17?(candidate)
+    end
   end
   
   # More methods will go below
+  def ordered_by_qualifications(candidates)
+    candidates.sort_by { |candidate| [-candidate[:years_of_experience], -candidate[:github_points]] }
+  end
+
+
+
--- main.rb
+++ main.rb
@@ -4,6 +4,41 @@ require 'pp'
 require './candidates'
 require './filters'
 
+puts "All candidates:"
+pp @candidates
+
+puts # blank line separator
+
 ## Your test code can go here
 
-pp @candidates
+# Test 1: Experienced candidates
+puts "Experienced Candidates - true or false:"
+puts "Is candidate number 1 experienced? #{experienced?(@candidates[0])}"
+puts
+
+# Test 2: Finding by :id
+id_to_find = 5
+found_candidate = find(id_to_find)
+puts "Candidate with ID #{id_to_find} found:"
+pp found_candidate # pp prints prettily (or something? it makes it nicer to look at!)
+
+puts
+
+#Test 3: Finding any qualified candidates
+qualified = qualified_candidates(@candidates)
+if qualified.any?
+  puts "Qualified Candidates:"
+  pp qualified
+else
+  puts "No one can cut it!"
+end
+puts
+
+#Test 4: Ordering candidates by qualifications
+ordered_candidates = ordered_by_qualifications(@candidates)
+if ordered_candidates.any?
+  puts "Ordered Candidates:"
+  pp ordered_candidates
+else
+  puts "Should have called Charli!"
+end
